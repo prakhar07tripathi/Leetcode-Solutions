6657947499ebebdff834a9e7707be49c8dd182eb
prakhar07tripathi <prakhar07tripathi@gmail.com>
2026-08-29 14:22:33 +0530

    Time: 15 ms (55.86%) | Memory: 23.3 MB (21.76%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,35 +2,31 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         // create a checklist for each char if is is present in the substring or not
-        int arr[256] = {0};
+        vector<int> check(256, -1);
         string sub = "";
         //initiate a start of the substring initially set to 0.
         int left = 0;
-        int i = 0;
+        int r = 0;
         int maxsize = 0;
         //sub.size() - left gives the length of the required substring it should be less than the s.size().
-        while(sub.size() - left < s.size() && i < s.size()){
+        while(r < s.size()){
             //if substr is empty append it directly
             if(!sub.size()){
-                arr[s[i]] = 1;
+                check[s[r]] = r;
                 }
             // else if the current element is not present in the substring append
-            else if(!arr[s[i]]){
-                arr[s[i]] = 1;
+            else if(check[s[r]] == -1){
+                check[s[r]] = r;
             }
             // else if the element is present reset the checklist and start a new subarray from the current element 
             else{
-                while(s[left] != s[i]){
-                    arr[s[left]] = 0;
-                    left++;
-                }
-                left++;
-                arr[s[i]] = 1;
+                if(left < check[s[r]] + 1)left = check[s[r]] + 1;
+                check[s[r]] = r;
             }
-            sub.push_back(s[i]);
+            sub.push_back(s[r]);
             //store the max of such subarrays 
-            if(sub.size() - left > maxsize)maxsize = sub.size() - left;
-            i++;
+            if(r - left + 1 > maxsize)maxsize = r - left + 1;
+            r++;
         }
         return maxsize;
     }
