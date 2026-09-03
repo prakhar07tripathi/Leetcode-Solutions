379d22548e68a1eae2061633bdf503fdde58affa
prakhar07tripathi <prakhar07tripathi@gmail.com>
2026-09-03 14:15:01 +0530

    Time: 4 ms (45.12%) | Memory: 34.9 MB (34.89%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,16 +1,24 @@
 class Solution {
-public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        unordered_map<int ,int>prefsum;
-        int sum = 0;
+    private:
+    int sumLessEqualGoal(vector<int> nums, int goal){
+        if(goal < 0)return 0;
+        int l = 0;
+        int r = 0;
         int cnt = 0;
-        for(int i = 0; i < nums.size(); i++){
-            prefsum[sum]++;
-            sum += nums[i];
-            if(prefsum.find(sum - goal) != prefsum.end()){
-                cnt += prefsum[sum - goal];
+        int sum = 0;
+        while(r < nums.size()){
+            sum += nums[r];
+            while(sum > goal){
+                sum -= nums[l];
+                l++;
             }
+            cnt += (r-l+1);
+            r++;
         }
         return cnt;
+    }
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return sumLessEqualGoal(nums, goal) - sumLessEqualGoal(nums, goal - 1);
     }
 };
