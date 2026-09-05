ed16a03e51e3b15efc1c4e4ccc5ec66fdc26a9be
prakhar07tripathi <prakhar07tripathi@gmail.com>
2026-09-05 10:47:58 +0530

    Time: 15 ms (39.37%) | Memory: 210.8 MB (35.81%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -1,27 +1,16 @@
 class Solution {
-    private:
-    vector<int> prefixmax(vector<int> nums){
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> prefmax(n);
+        vector<int> prefmax(n), suffmin(n);
         prefmax[0] = nums[0];
         for(int i = 1; i < n; i++){
             prefmax[i] = max(prefmax[i-1], nums[i]); 
         }
-        return prefmax;
-    }
-    vector<int> suffixmin(vector<int> nums){
-        int n = nums.size();
-        vector<int> suffmin(n);
         suffmin[n-1] = nums[n-1];
         for(int i = n-2; i >= 0; i--){
             suffmin[i] = min(suffmin[i+1], nums[i]); 
         }
-        return suffmin;
-    }
-public:
-    int firstStableIndex(vector<int>& nums, int k) {
-        vector<int>prefmax = prefixmax(nums);
-        vector<int>suffmin = suffixmin(nums);
         for(int i = 0; i < nums.size(); i++){
             if(prefmax[i] - suffmin[i] <= k){
                 return i;
