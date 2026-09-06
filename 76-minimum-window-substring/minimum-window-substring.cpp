d421392b3353cd833dbe488fc0fadbb871c66d05
prakhar07tripathi <prakhar07tripathi@gmail.com>
2026-09-06 13:12:54 +0530

    Time: 11 ms (47.36%) | Memory: 11.3 MB (94.31%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char, int>tmap1;
+        int req = t.size();
+        int arr[2] = {0,-1};
+        int r = 0,l = 0, minlen = INT_MAX;
+        for(int i = 0; i < t.size(); i++){
+            tmap1[t[i]]++;
+        }
+        if(!tmap1.size())return "";
+        while(r < s.size()){
+            if(tmap1.find(s[r]) != tmap1.end()){
+                if(tmap1[s[r]] > 0){
+                req--;
+                }
+                tmap1[s[r]]--;
+            }
+            while(l <= r && req <= 0){
+                if(minlen > (r-l+1)){
+                arr[0] = l;
+                arr[1] = r;
+                minlen = (r-l+1);
+                }
+                if(tmap1.find(s[l]) != tmap1.end()){
+                    if(tmap1[s[l]] >= 0){
+                        req++;
+                    }
+                    tmap1[s[l]]++;
+                    }
+                l++;
+            }
+            r++;
+        }
+        if(minlen == INT_MAX)return "";
+        return s.substr(arr[0], arr[1]-arr[0] + 1);
+    }
+};
